0fec69c3086a7dab95f3b02b3cd2d1f7e37c5680
Kevin Andrade <kandrad2@binghamton.edu>
2019-01-30 01:57:37 -0500

    attempting to make webpage open

--- Project1/app/src/main/java/com/example/project1/MainActivity.java
+++ Project1/app/src/main/java/com/example/project1/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.project1;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,13 +19,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+/*
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "This is interesting", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+*/
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 Intent i = new Intent(Intent.ACTION_VIEW);
+                 i.setData(Uri.parse("https://www.gmail.com"));
+                startActivity(i);﻿
             }
         });
     }
