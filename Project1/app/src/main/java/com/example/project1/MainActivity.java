e2ae8ba67ff32c6755d81d5fa369600b04267f01
Kevin Andrade <kandrad2@binghamton.edu>
2019-01-30 22:35:45 -0500

    Added text generator with button click

--- Project1/app/src/main/java/com/example/project1/MainActivity.java
+++ Project1/app/src/main/java/com/example/project1/MainActivity.java
@@ -11,9 +11,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+      int tracker = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        final TextView text = findViewById(R.id.textView);
+        Button btn = findViewById(R.id.button);
 /*
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -31,16 +36,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 */
+        final String [] jokes = new String [6];
+        jokes[0] = "What's the best thing about switzerland?";
+        jokes[1] = "I don't know but the flag is a big plus.";
+        jokes[2] = "Did you hear about the actor who fell through the floorboards?";
+        jokes[3] = "He was just going through a stage.";
+        jokes[4] = "Why don't scientists trust atoms?";
+        jokes[5] = "Because they make up everything";
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                text.setText(jokes[tracker]);
+                tracker++;
+                if(tracker > 5) tracker = 0;
+
+            }
+        });
+
         ImageView fab = (ImageView)findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                  Intent i = new Intent(Intent.ACTION_VIEW);
                  i.addCategory(Intent.CATEGORY_BROWSABLE);
-                 i.setData(Uri.parse("https://www.facebook.com"));
+                 i.setData(Uri.parse("https://www.gmail.com"));
                  startActivity(i);
             }
         });
+
+
     }
 
     @Override
